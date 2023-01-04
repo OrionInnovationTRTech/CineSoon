bf78f8b60f3418b14595e7b28ae295af64229585
Emin Saygı <emin_0001@hotmail.com>
2023-01-04 14:55:43 +0300

    Session Model added

---
+++ TMDB-App/Model/SessionID.swift
@@ -0,0 +1,13 @@
+//
+//  SessionID.swift
+//  TMDB-App
+//
+//  Created by Emin Saygı on 20.12.2022.
+//
+
+import Foundation
+
+struct SessionID: Codable {
+    let success: Bool
+    let sessionID: String
+}
